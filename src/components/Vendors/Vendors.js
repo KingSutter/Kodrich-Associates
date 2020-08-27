724ae6f0faa092a21408bcfb804b10a80d68fc30
Will Sutter <KingSutter12@gmail.com>
2020-08-26 21:05:57 -0500

    finish vendor overview page

--- src/components/Vendors/Vendors.js
+++ src/components/Vendors/Vendors.js
@@ -5,7 +5,7 @@ class Vendors extends Component {
     render() {
         return (
             <div className="vendors-page">
-                <table>
+                <table width="90%" border="0" cellpadding="30%">
                     <tr>
                         <td>
                             <img src="img/Coriander-logo.png" alt="Coriander logo"/>
@@ -27,7 +27,7 @@ class Vendors extends Component {
                             <img src="img/Standup-logo.png" alt="Standup logo"/>
                         </td>
                         <td>
-                            Stand Up overview
+                            Empowering employees to transition from seated to standing positions throughout the day can help increase productivity and improve employee wellness. With mechanisms for all major office furniture manufacturers, standUP makes it easy for designers and dealers to spec standUP’s sit/stand mechanisms into new projects, or retrofit existing office furniture.
                         </td>
                     </tr>
                     <tr>
@@ -35,7 +35,7 @@ class Vendors extends Component {
                             <img src="img/Invincible-logo.png" alt="Invincible logo"/>
                         </td>
                         <td>
-                            Invincible overview
+                            We are pleased to offer you some of the highest quality office furniture on the market. Invincible™ Office Furniture Solutions is proud to say our products; including our workstations, filing and storage solutions, systems furniture, and panel systems are 100% U.S. manufactured. Invincible™’s legendary “made in America” quality is supported by over a century of office furniture manufacturing experience and backed up by our lifetime warranty. When you buy Invincible™, you can know that we are “building forever furniture” just for you.
                         </td>
                     </tr>
                     <tr>
@@ -43,7 +43,7 @@ class Vendors extends Component {
                             <img src="img/Afra-logo.png" alt="Afra logo"/>
                         </td>
                         <td>
-                            Afra overview
+                            AFRA furniture offers a commercial and industrial furniture design representing comfort and innovation, using a broad spectrum of materials, technologies, finishes and colors. We are focused on diversity in design that is suitable for every project and environment. Our goal is to improve our quality of life, by supporting sustainability, innovation and training. AFRA is continuously looking to offer durability, purpose and harmony in all of our furniture.
                         </td>
                     </tr>
                 </table>
