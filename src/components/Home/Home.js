ef5744be6fbb9a457a85eb4e09406edb481cdb99
Will Sutter <KingSutter12@gmail.com>
2020-10-28 20:02:08 -0500

    add new homepage img and fix AFRA page bug
    also fixed some broken links

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,6 +9,24 @@ const fadeImages = [
   'img/home4.jpeg',
   'img/home5.jpeg',
   'img/home6.jpeg',
+  'img/home.jpg',
+  'img/home2.jpg',
+  'img/home3.jpg',
+  'img/home7.jpg',
+  'img/home8.jpg',
+  'img/home9.jpg',
+  'img/home10.jpg',
+  'img/home11.jpg',
+  'img/home12.jpg',
+  'img/home13.jpg',
+  'img/home14.jpg',
+  'img/home15.jpg',
+  'img/home16.jpg',
+  'img/home17.jpg',
+  'img/home18.jpg',
+  'img/home19.jpg',
+  'img/home20.jpg',
+  'img/home21.jpg',
 ];
 
 class UserPage extends Component {
@@ -42,6 +60,100 @@ class UserPage extends Component {
             <div className="each-fade">
               <div className="image-container">
                 <img src={fadeImages[2]} alt="slide image 3" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[3]} alt="slide image 4"/>
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[4]} alt="slide image 5" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[5]} alt="slide image 6" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[6]} alt="slide image 7"/>
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[7]} alt="slide image 8" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[8]} alt="slide image 9" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[9]} alt="slide image 10"/>
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[10]} alt="slide image 11" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[11]} alt="slide image 12" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[12]} alt="slide image 13"/>
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[13]} alt="slide image 14" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[14]} alt="slide image 15" />
+              </div>
+            </div><div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[15]} alt="slide image 16"/>
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[16]} alt="slide image 17" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[17]} alt="slide image 18" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[18]} alt="slide image 19"/>
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[19]} alt="slide image 20" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[20]} alt="slide image 21" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={fadeImages[21]} alt="slide image 22" />
               </div>
             </div>
           </Fade>
