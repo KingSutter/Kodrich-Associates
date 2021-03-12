d2b3decee3cd9f0c1ddb87cb88e779c2ee90b328
Will Sutter <KingSutter12@gmail.com>
2021-03-12 00:26:09 -0700

    Finished CSS of message form
    Other minor tweaks and code comments added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import axios from 'axios';
 import './Contact.css';
-import {FromControl} from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { green, purple } from '@material-ui/core/colors';
+import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 const MicRecorder = require('mic-recorder-to-mp3');
 
 const Mp3Recorder = new MicRecorder({bitrate: 128});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#00acb0',
+      main: '#00acb0',
+      dark: '#008183',
+      contrastText: '#fff',
+    },
+  }
+});
 
 class Contact extends React.Component {
   constructor(props) {
@@ -21,10 +34,9 @@ class Contact extends React.Component {
     }
   }
   
-  // Mp3Recorder = new MicRecorder({bitrate: 128});
-
   handleSubmit() {
-    // Sends email to Kodrich Associates
+    // Sends email to Kodrich Associates ... then resets form'
+    this.resetForm();
   }
 
   // Asks user for microphone permission
@@ -62,7 +74,6 @@ class Contact extends React.Component {
       .getMp3()
       .then(([buffer, blob]) => {
         const blobURL = URL.createObjectURL(blob)
-        var axios = require('axios');
         // begin file upload to API
         var data = blob;
 
@@ -83,7 +94,6 @@ class Contact extends React.Component {
           console.log("Upload URL" + response.data.upload_url);
           // Request uploaded audio file for transcription to API
           console.log("first request complete, starting second...");
-          var axios = require('axios');
           var data = JSON.stringify({"audio_src_url":response.data.upload_url});
 
           var config = {
@@ -101,10 +111,8 @@ class Contact extends React.Component {
           .then((response) => {
             console.log(JSON.stringify(response.data));
             console.log("second request complete, starting third");
-            // Request trascribed audio from API
+            // Request trascribed audio from API after 15 seconds
             setTimeout(() => {;
-              var axios = require('axios');
-
               var config = {
                 method: 'get',
                 url: `https://api.assemblyai.com/transcript/${response.data.transcript.id}`,
@@ -137,6 +145,18 @@ class Contact extends React.Component {
       }).catch((e) => console.log(e));
   };
 
+  onNameChange(event) {
+    this.setState({ name: event.target.value })
+  }
+
+  onEmailChange(event) {
+    this.setState({ email: event.target.value })
+  }
+
+  onMessageChange(event) {
+    this.setState({ message: event.target.value })
+  }
+  
   resetForm() {
     this.setState({ name: '', email: '', message: '' })
   }
@@ -149,75 +169,87 @@ class Contact extends React.Component {
           <p>Please feel free to contact us at any time and we'll do our best to get back to you as soon as possible during normal business hours.</p>
         </div>
         <div className="flex-container">
-          <div className="form-container">
-            <form id="contact-form">
-              <div className="form-group">
-                <TextField
-                  required
-                  id="filled-required"
-                  label="Required"
-                  defaultValue="Hello World"
-                  placeholder="Name"
-                  variant="filled"
-                />
-                <input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email</label>
-                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-              </div>
-              <div className="form-group">
-                <label htmlFor="message">Message</label>
-                <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-              </div>
-              {/* record button */}
-              <button onClick={this.start} disabled={this.state.isRecording}>
-                Record
-              </button>
-              <button onClick={this.stop} disabled={!this.state.isRecording}>
-                Stop
-              </button><br/><br/>
-              <button type="submit" className="btn btn-primary" onClick={()=>this.handleSubmit()}>Submit</button>
-              {/* <audio src={this.state.blobURL} controls="controls" /> */}
-            </form>
-          </div>
+          {/* Google Maps embed and contact information */}
           <div className="info-container">
-            <h4>Our address:</h4><br/>
-            {/* Interactive map below, remove the comment code and brackets to reactivate it */}
-            {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2824.7114898087175!2d-93.46421218424635!3d44.929202377001765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f61e357c38818f%3A0x5b0e059e915bb0fe!2s14333%20Oakwood%20Rd%2C%20Minnetonka%2C%20MN%2055345!5e0!3m2!1sen!2sus!4v1601166390742!5m2!1sen!2sus" 
-              width="400" 
-              height="300" 
-              frameborder="0" 
-              style={{border:"0;"}} 
-              allowfullscreen="" 
-              aria-hidden="false" 
-              tabindex="0">
-            </iframe>
-            <br/> */}
-            <a href="https://goo.gl/maps/wdAusCJoAjLz9pRT8" target="_blank" rel="noopener noreferrer">14333 Oakwood Road<br/>Minnetonka, MN 55345</a>
-            <br/><br/>
-            <b>Phone: </b><a href="tel:6127162352">612-716-2352 </a>
-            <br/><br/>
-            <b>Email: </b><a href="mailto:kodrich@hotmail.com">kodrich@hotmail.com</a><br/><br/>
-            <b>Hours: </b>
-            <div>Monday to Friday, 9am - 5pm</div>
-          </div>
+              <h4>Our address:</h4><br/>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2824.7114898087175!2d-93.46421218424635!3d44.929202377001765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f61e357c38818f%3A0x5b0e059e915bb0fe!2s14333%20Oakwood%20Rd%2C%20Minnetonka%2C%20MN%2055345!5e0!3m2!1sen!2sus!4v1601166390742!5m2!1sen!2sus" 
+                width="400" 
+                height="300" 
+                frameborder="0" 
+                style={{border:"0;"}} 
+                allowfullscreen="" 
+                aria-hidden="false" 
+                tabindex="0">
+              </iframe>
+              <br/>
+              <a href="https://goo.gl/maps/wdAusCJoAjLz9pRT8" target="_blank" rel="noopener noreferrer">14333 Oakwood Road<br/>Minnetonka, MN 55345</a>
+              <br/><br/>
+              <b>Phone: </b><a href="tel:6127162352">612-716-2352 </a>
+              <br/><br/>
+              <b>Email: </b><a href="mailto:kodrich@hotmail.com">kodrich@hotmail.com</a><br/><br/>
+              <b>Hours: </b>
+              <div>Monday to Friday, 9am - 5pm</div>
+            </div>
+          {/* Form for sending an email */}
+          <ThemeProvider theme={theme}> 
+            <div className="form-container">
+                <h4>Send us a message:</h4>
+                <div className="form-group">
+                  <TextField
+                    required
+                    id="filled-required"
+                    label="Name"
+                    defaultValue="Hello World"
+                    placeholder="John/Jane Doe"
+                    variant="filled"
+                    value={this.state.name} onChange={this.onNameChange.bind(this)}
+                  />
+                </div>
+                <div className="form-group">
+                <TextField
+                    required
+                    id="filled-required"
+                    label="Email"
+                    required
+                    defaultValue="Hello World"
+                    placeholder="example@gmail.com"
+                    variant="filled"
+                    value={this.state.email} onChange={this.onEmailChange.bind(this)}
+                  />
+                </div>
+                <div className="form-group">
+                    <TextField
+                    id="filled-multiline-flexible"
+                    label="Message"
+                    multiline
+                    required
+                    rows={5}
+                    rowsMax={7}
+                    variant="filled"
+                    fullWidth={true}
+                    value={this.state.message} 
+                    onChange={this.onMessageChange.bind(this)}
+                    />
+                </div>
+                {/* record button */}
+                  <Button className="record-buttons" onClick={this.start} disabled={this.state.isRecording} variant="contained" color="primary">
+                    Record
+                  </Button>
+                  
+                  {/* stop button */}
+                  <Button className="record-buttons" onClick={this.stop} disabled={!this.state.isRecording} variant="contained" color="primary">
+                    Stop
+                  </Button><br/><br/>
+                  <Button type="submit" variant="contained" color="primary" onClick={()=>this.handleSubmit()}>Submit</Button>
+                
+                {/* The commented out code below shows the recorded audio file. Will save for later if needed */}
+                {/* <audio src={this.state.blobURL} controls="controls" /> */}
+            </div>
+          </ThemeProvider>
         </div>
         {/* <pre>{JSON.stringify(this.state, null, 2)}</pre> */}
       </div >
     );
-  }
-
-  onNameChange(event) {
-    this.setState({ name: event.target.value })
-  }
-
-  onEmailChange(event) {
-    this.setState({ email: event.target.value })
-  }
-
-  onMessageChange(event) {
-    this.setState({ message: event.target.value })
   }
 }
 
